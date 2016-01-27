b8409a46a7d28238f84cf2dba2821de4786cd766
Sylvain Abadie <sylvain.abadie@insa-lyon.fr>
2016-01-27 18:15:23 +0100

    Extended support for striping function which could contain strings with closing parenthesis inside like :  console.log(' );' );  and unit tests.

--- tests/fixtures/app/index.js
+++ tests/fixtures/app/index.js
@@ -1,14 +1,15 @@
-console.log('a console.log on the first line should get stripped');
+console.log( 'a console.log on the first line should get stripped' );
 /**
  * Copyright 2015, Yahoo! Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 
-var makeFoo = function (bar, baz) {
+var makeFoo = function ( bar, baz ) {
     // The following 2 lines of code will be stripped with our webpack loader
-    console.log('some debug info');
-    debug('better debug info');
+    console.log( 'some debug info' );
+    debug( 'better debug info' );
+    console.log( 'some' + "debug " + info + '(even closing parenthesis);' );
     // This code would remain
-    return new Foo(bar, baz);
+    return new Foo( bar, baz );
 };
-console.log('a console.log on the last line without a semicolon should get stripped')
+console.log( 'a console.log on the last line without a semicolon should get stripped' )
